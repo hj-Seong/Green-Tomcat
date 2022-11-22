88b4a12167f6b3e69d483690cddd39fc32784617
hj-Seong <hjseong1222@gmail.com>
2022-11-22 11:17:23 +0900

    1122 DB연결- 실습1 DAO 확인

--- greenTest/src/main/java/book/BookDAO.java
+++ greenTest/src/main/java/book/BookDAO.java
@@ -1,11 +1,28 @@
 package book;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import utill.DatabaseUtill;
+
 public class BookDAO {
 	// addBook을 통해서 insert 쿼리문 작성
 	public int addBook (String title, int count) {
 		// SQL구문을 작성후
+		String SQL = "insert into Book values (?,?)";
+		
 		// try-catch를 통해서 DB와 연결을 한다
 		// > UserDAO 참고하여 작성
+		try {
+			Connection conn = DatabaseUtill.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, title);
+			pstmt.setInt(2, count);
+			return pstmt.executeUpdate(); // 성공했을 때 1반환
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 		return -1;
 	}
---
+++ greenTest/src/main/webapp/bookAddAction.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" 
+    contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<%  
+// userJoinAction.jsp를 참고하셔서
+// DTO와 DAO를 사용하여 데이터베이스 값 넣기 *int자료형 주의
+%>
+
